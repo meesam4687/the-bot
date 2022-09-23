83d271dc5cd5fd087991c41896bf0186f022b783
meesam4687 <meesam4687@gmail.com>
2022-09-23 20:27:00 +0530

    Added Remove

---
+++ commands/remove.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    data: new Discord.SlashCommandBuilder()
+        .setName('remove')
+        .setDescription('Remove a song from the queue')
+        .addIntegerOption(option => option.setName('int').setDescription('Number in queue')),
+    async execute(interaction) {
+        let opt = interaction.options.getInteger('int');
+        const queue = interaction.client.distube.getQueue(interaction)
+        if (!queue) return interaction.reply({content: `Nothing is Being Played`, ephemeral: true}).catch(console.error)
+        if(queue.length<opt) return interaction.reply("Was quite funny that you tried to do that");
+        let removed = queue.songs.splice(opt, opt);
+        interaction.reply(`Removed \`${removed.name}\` from the queue`).catch(console.error)
+    },
+};
