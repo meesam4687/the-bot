ac8509397b94886e40f42f1efd07d996a98c3ad0
meesam4687 <meesam4687@gmail.com>
2022-09-23 21:04:27 +0530

    e

--- commands/resume.js
+++ commands/resume.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 
 module.exports = {
-	data: new Discord.SlashCommandBuilder()
-		.setName('resume')
-		.setDescription('Resume the queue'),
-	async execute(interaction) {
-        const queue = interaction.client.distube.getQueue(interaction)
-        if (!queue) return interaction.reply(`You never played anything in the first place`).catch(console.error)
-        if(!queue.paused) return interaction.reply('Its not paused').catch(console.error)
-        queue.resume()
-        interaction.reply("Resumed").catch(console.error)
-	},
+        data: new Discord.SlashCommandBuilder()
+                .setName('resume')
+                .setDescription('Resume the queue'),
+        async execute(interaction) {
+                const queue = interaction.client.distube.getQueue(interaction)
+                if (!queue) return interaction.reply(`You never played anything in the first place`).catch(console.error)
+                if (!queue.paused) return interaction.reply('Its not paused').catch(console.error)
+                queue.resume()
+                interaction.reply("Resumed").catch(console.error)
+        },
 };
