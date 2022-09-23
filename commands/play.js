f5be8b9f15adc0f90adb13429d17df79faff5cec
meesam4687 <meesam4687@gmail.com>
2022-09-23 20:53:05 +0530

    Added Remove

--- commands/play.js
+++ commands/play.js
@@ -15,6 +15,6 @@ module.exports = {
             textChannel: interaction.channel,
             interaction
         })
-        interaction.reply({content: "Now Playing"})
+        interaction.reply({content: "Added"})
     },
 };
--- commands/remove.js
+++ commands/remove.js
@@ -4,13 +4,13 @@ module.exports = {
     data: new Discord.SlashCommandBuilder()
         .setName('remove')
         .setDescription('Remove a song from the queue')
-        .addIntegerOption(option => option.setName('int').setDescription('Number in queue')),
+        .addIntegerOption(option => option.setName('int').setDescription('Number in queue').setRequired(true)),
     async execute(interaction) {
         let opt = interaction.options.getInteger('int');
         const queue = interaction.client.distube.getQueue(interaction)
         if (!queue) return interaction.reply({content: `Nothing is Being Played`, ephemeral: true}).catch(console.error)
         if(queue.length<opt) return interaction.reply("Was quite funny that you tried to do that");
         let removed = queue.songs.splice(opt, opt);
-        interaction.reply(`Removed \`${removed.name}\` from the queue`).catch(console.error)
+        interaction.reply(`Removed from the queue`).catch(console.error)
     },
 };
