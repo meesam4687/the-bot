12fe8a51e761e1f4090bf3a671626b6e76495e29
meesam4687 <meesam4687@gmail.com>
2022-09-23 21:09:04 +0530

    Added Help

---
+++ commands/help.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    data: new Discord.SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Get some help'),
+    async execute(interaction) {
+        const p = "/"
+        const infoEmbed = new Discord.EmbedBuilder()
+            .setColor('#57a3bd')
+            .setTitle('Help')
+            .setURL('https://www.youtube.com/watch?v=0iaNqJN5MTM')
+            .setDescription(`
+            Commands Available :
+          `)
+            .addFields(
+                { name: '━━ 🎶 ・ Music commands ━━', value: `\`${p}play\`, \`${p}stop\`, \`${p}skip\`, \`${p}forceskip\`, \`${p}queue\`, \`${p}nowplaying\`, \`${p}pause\`, \`${p}resume\`, \`${p}remove\`` },
+                { name: '឵឵  ឵឵  ឵឵', value: '឵឵  ឵឵  ឵឵' },
+                { name: '━━ 🤖 ・ General commands ━━', value: `\`${p}ping\`, \`${p}help\`` },
+            )
+        interaction.reply({ embeds: [infoEmbed] }).catch(console.error)
+    },
+};
