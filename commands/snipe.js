5f3a1cfb2fad6e3d0109cad8ad835efd2bb43229
Meesam <70945820+meesam4687@users.noreply.github.com>
2022-11-06 10:07:00 +0530

    Create snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+module.exports = {
+	data: new Discord.SlashCommandBuilder()
+		.setName('snipe')
+		.setDescription('Fetch the most recently deleted message in this channel'),
+	async execute(interaction) {
+    let msg = db.get(`snipemsg_${interaction.channel.id}`)
+    let senderid = db.get(`snipesender_${interaction.channel.id}`)
+    let pfp = db.get(`snipepfp_${interaction.channel.id}`)
+    var mesg = msg
+    if(!msg) {
+      return interaction.reply({content: `Well I Did Not Find Any Deleted Message In This Channel, Lol`, ephemeral: true})
+    }
+    if(msg.length > 200){
+      var mesg = `${msg.substr(0, 200)}.......`;
+    }
+    let embed = new Discord.MessageEmbed()
+     .setAuthor(db.get(`snipesender_${interaction.channel.id}`), pfp)
+     .setDescription(mesg.replace(/(https?:\/\/)?(www\.)?((discordapp\.com\/invite)|(discord\.gg))\/(\w+)|https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g , "*link*"))
+     .setFooter({ text: 'Sniped, lol' })
+    interaction.reply({ embeds: [embed] })
+	},
+};
